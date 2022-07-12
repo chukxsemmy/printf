cc6952b9a04904b7d734e1f43a838093d4fe873b
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-12 09:03:21 +0800

    print f and main

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_printf - printf
+ *@format: const char pointer
+ *Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	const char *string;
+	int cont = 0;
+	va_list arg;
+
+	if (!format)
+		return (-1);
+
+	va_start(arg, format);
+	string = format;
+
+	cont = loop_format(arg, string);
+
+	va_end(arg);
+	return (cont);
+}
