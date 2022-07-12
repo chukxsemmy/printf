9f254fcf06714d0bc76bb4308d4d9595e8195387
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-12 09:09:16 +0800

    fix for printf

--- main.h
+++ main.h
@@ -7,5 +7,8 @@ int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
 int _printf(const char *format, ...);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
 
 #endif
